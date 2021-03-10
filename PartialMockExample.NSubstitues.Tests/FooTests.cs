63ff5bfdc360452242c16e4428052ce507116e9e
Scott Williams <thundereagle@yahoo.com>
2021-03-10 09:07:14 -0500

    remvoed athena_bin output folder

--- PartialMockExample.NSubstitues.Tests/FooTests.cs
+++ PartialMockExample.NSubstitues.Tests/FooTests.cs
@@ -8,151 +8,123 @@ using System.Threading.Tasks;
 using NSubstitute;
 using NUnit.Framework;
 
-namespace PartialMockExample.NSubstitue.Tests
-{
-	[TestFixture]
-	public class FooTests
-	{
+namespace PartialMockExample.NSubstitue.Tests {
+  [TestFixture]
+  public class FooTests {
+    [Test]
+    public void RealCodeExecuting() {
+      var foo = new Foo();
 
-		[Test]
-		public void RealCodeExecuting()
-		{
-			var foo = new Foo();
+      foo.DoSomething();
+      foo.IsValid();
+      foo.GetValue(2, 2);
+      foo.GetValue2(2, 2);
+      foo.BusinessLogicMethod(2);
+    }
 
-			foo.DoSomething();
-			foo.IsValid();
-			foo.GetValue(2, 2);
-			foo.GetValue2(2, 2);
-			foo.BusinessLogicMethod(2);
+    [Test]
+    public void PartialMock_StillRealCode() {
+      var foo = Substitute.ForPartsOf<Foo>();
 
-		}
+      foo.DoSomething();
+      foo.IsValid();
+      foo.GetValue(2, 2);
+      foo.GetValue2(2, 2);
+      foo.BusinessLogicMethod(2);
+    }
 
-		[Test]
-		public void PartialMock_StillRealCode()
-		{
+    [Test]
+    public void StubAVoidMethod() {
+      var foo = Substitute.ForPartsOf<Foo>();
 
-			var foo = Substitute.ForPartsOf<Foo>();
+      foo.When(f => f.DoSomething()).DoNotCallBase();
+      foo.DoSomething();
 
-			foo.DoSomething();
-			foo.IsValid();
-			foo.GetValue(2, 2);
-			foo.GetValue2(2, 2);
-			foo.BusinessLogicMethod(2);
+      foo.IsValid();
+    }
 
-		}
+    [Test]
+    public void StubVirual() {
+      var foo = Substitute.ForPartsOf<Foo>();
+      foo.When(f => f.IsValid()).DoNotCallBase();
+      foo.IsValid().Returns(true);
+      var actual = foo.IsValid();
+      Assert.IsTrue(actual);
+    }
 
-		[Test]
-		public void StubAVoidMethod()
-		{
+    [Test]
+    public void StubNonVirual() {
+      //note, this won't work
+      var foo = Substitute.ForPartsOf<Foo>();
+      foo.GetValue(Arg.Is(2), Arg.Is(2)).Returns(10);
+      foo.GetValue(2, 2);
+    }
 
-			var foo = Substitute.ForPartsOf<Foo>();
+    [Test]
+    public void StubInternalMethod() {
+      var foo = Substitute.ForPartsOf<Foo>();
 
-			foo.When(f => f.DoSomething()).DoNotCallBase();
-			foo.DoSomething();
+      //foo.GetValue2(Arg.Is(2), Arg.Is(4)).Returns(10);
 
-			foo.IsValid();
+      //foo.When(f => f.GetValue2(Arg.Any<int>(), Arg.Any<int>())).DoNotCallBase();
+      foo.WhenForAnyArgs(f => f.GetValue2(0, 0)).DoNotCallBase();
+      //foo.GetValue2(2, 2).Returns(10);
+      foo.GetValue2(2, 4).Returns(10);
 
-		}
+      var actual = foo.BusinessLogicMethod(2);
+      Assert.AreEqual(10, actual);
+    }
 
-		[Test]
-		public void StubVirual()
-		{
-			var foo = Substitute.ForPartsOf<Foo>();
-			foo.When(f => f.IsValid()).DoNotCallBase();
-			foo.IsValid().Returns(true);
-			var actual = foo.IsValid();
-			Assert.IsTrue(actual);
-		}
+    [Test]
+    public void TryParseSub() {
+      var foo = Substitute.ForPartsOf<Foo>();
+      int result;
+      foo.WhenForAnyArgs(f => f.TryParse(string.Empty, out result)).DoNotCallBase();
 
-		[Test]
-		public void StubNonVirual()
-		{
-			//note, this won't work
-			var foo = Substitute.ForPartsOf<Foo>();
-			foo.GetValue(Arg.Is(2), Arg.Is(2)).Returns(10);
-			foo.GetValue(2, 2);
-		}
+      foo.TryParse("3", out result).Returns(x => {
+        x[1] = 5;
+        return true;
+      });
+    }
 
-		[Test]
-		public void StubInternalMethod()
-		{
-			var foo = Substitute.ForPartsOf<Foo>();
+    [Test]
+    public void DoIt() {
+      var foo = Substitute.ForPartsOf<Foo>();
+      bool cancel;
 
-			//foo.GetValue2(Arg.Is(2), Arg.Is(4)).Returns(10);
+      foo.WhenForAnyArgs(f => f.Bar(out cancel)).DoNotCallBase();
+      foo.WhenForAnyArgs(f => f.Bar(out cancel)).Do(x => { x[0] = true; });
 
-			//foo.When(f => f.GetValue2(Arg.Any<int>(), Arg.Any<int>())).DoNotCallBase();
-			foo.WhenForAnyArgs(f => f.GetValue2(0, 0)).DoNotCallBase();
-			//foo.GetValue2(2, 2).Returns(10);
-			foo.GetValue2(2, 4).Returns(10);
+      foo.Bar(out cancel);
+      Assert.IsTrue(cancel);
+    }
 
-			var actual = foo.BusinessLogicMethod(2);
-			Assert.AreEqual(10, actual);
-		}
+    [Test]
+    public void DoIt2() {
+      var foo = Substitute.ForPartsOf<Foo>();
+      int count;
 
+      foo.WhenForAnyArgs(f => f.MultiPartial(out count)).DoNotCallBase();
+      //foo.When(f => f.MultiPartial(out count)).DoNotCallBase();
+      //foo.When(f => f.MultiPartial(out count)).DoNotCallBase();
 
+      foo.MultiPartial(out count).Returns(x => {
+        x[0] = 5;
+        return false;
+      });
 
-		[Test]
-		public void TryParseSub()
-		{
-			var foo = Substitute.ForPartsOf<Foo>();
-			int result;
-			foo.WhenForAnyArgs(f => f.TryParse(string.Empty, out result)).DoNotCallBase();
+      var actual = foo.MultiPartial(out count);
+      var actual2 = foo.MultiPartial(out count);
+      Assert.IsFalse(actual2);
+      Assert.AreEqual(5, count);
+    }
 
-			foo.TryParse("3", out result).Returns(x =>
-			{
-				x[1] = 5;
-				return true;
-			});
-		}
-
-		[Test]
-		public void DoIt()
-		{
-
-			var foo = Substitute.ForPartsOf<Foo>();
-			bool cancel;
-
-			foo.WhenForAnyArgs(f => f.Bar(out cancel)).DoNotCallBase();
-			foo.WhenForAnyArgs(f => f.Bar(out cancel)).Do(x => { x[0] = true; });
-
-			foo.Bar(out cancel);
-			Assert.IsTrue(cancel);
-
-		}
-
-		[Test]
-		public void DoIt2()
-		{
-
-			var foo = Substitute.ForPartsOf<Foo>();
-			int count;
-			
-
-			foo.WhenForAnyArgs(f => f.MultiPartial(out count)).DoNotCallBase();
-			//foo.When(f => f.MultiPartial(out count)).DoNotCallBase();
-			//foo.When(f => f.MultiPartial(out count)).DoNotCallBase();
-			
-			foo.MultiPartial(out count).Returns(x =>
-			{
-				x[0] = 5;
-				return false;
-			});
-
-			var actual = foo.MultiPartial(out count);
-			var actual2 = foo.MultiPartial(out count);
-			Assert.IsFalse(actual2);
-			Assert.AreEqual(5, count);
-
-		}
-
-
-
-		[Test]
-		public void ReadOnlyPropertyTest()
-		{
-			var foo = Substitute.ForPartsOf<Foo>();
-			foo.When(x => { var temp = x.MyProp; }).DoNotCallBase();
-		}
-
-	}
+    [Test]
+    public void ReadOnlyPropertyTest() {
+      var foo = Substitute.ForPartsOf<Foo>();
+      foo.When(x => {
+        var temp = x.MyProp;
+      }).DoNotCallBase();
+    }
+  }
 }
